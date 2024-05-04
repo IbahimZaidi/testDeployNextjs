f6ad6607498d9c68bad8865a46aaebd7eb4b4672
brahim <brahim@gmail.com>
2024-05-04 16:06:33 +0100

    after showing the data inside the main div of the index page , and in the console

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,38 @@
-import Image from "next/image";
+"use client";
+import { getDataStudents } from "@/helperFunction/getDataStudents";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // inset the data into a state :
+    getDataStudents().then((resolve) => {
+      setData(resolve);
+    });
+  }, []);
+
+  useEffect(() => {
+    // print the data in the console to cheak it before use it in the main div component :
+    console.log("############### , the data is : ", data);
+  }, [data]);
+
+  // the return of the component :
   return (
-    <main className="h-100% flex min-h-screen flex-col items-center justify-between p-24">
-      hello from the next js application
+    <main className=" grid grid-cols-3 space-y-4 bg-green-500  min-h-screen  p-24">
+      {data.length > 0 ? (
+        data.map((elem, index) => {
+          return (
+            <div
+              key={index}
+              className=" bg-yellow-300 h-10 w-200px p-3 flex justify-center items-center border border-black  "
+            >
+              {elem.first_name}
+            </div>
+          );
+        })
+      ) : (
+        <div>is loading ....... </div>
+      )}
     </main>
   );
 }
