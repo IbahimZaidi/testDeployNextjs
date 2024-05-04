1bcf05343dc7881f2ac47e2df2445a1392f216d0
brahim <brahim@gmail.com>
2024-05-04 16:32:50 +0100

    change the link in the functino how fetch url , change url from https://localhost/api... to /api/..

--- src/helperFunction/getDataStudents.jsx
+++ src/helperFunction/getDataStudents.jsx
@@ -1,5 +1,5 @@
 export const getDataStudents = async () => {
-  const response = await fetch("http://localhost:3001/api/testDeploy");
+  const response = await fetch("/api/testDeploy");
   const result = await response.json();
 
   return result.students;
